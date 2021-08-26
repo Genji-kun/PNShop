ac4cafb7f59dc365bc822e2fc20c7b47c08e0bc3
Genji-kun <87120829+Genji-kun@users.noreply.github.com>
2021-08-26 13:23:36 +0700

    hehe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     $("#go-to-top").hide()
+    $(".info").hide()
+    $("#text").hide()
 
+
+    $(".item").click(function() {
+        $(".info img").attr("src",$(this).children('a').children('img').attr('src'))
+        $(".info h1").text($(this).children('a').children('h2').text()) 
+        $(".info h3").text($(this).children('a').children('h3').text())
+        $(".info p").text($(this).children('a').children('p').text())
+        $(".info").fadeIn(500)
+        $("#text").fadeIn()
+    })
+
+    $(".exit").click(function() {
+        $(".info").fadeOut(500)
+        $("#text").fadeOut()
+    })
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 435)
             $("#go-to-top").show("slow")
@@ -12,5 +28,4 @@ $(document).ready(function() {
             scrollTop:0
         }, 1000);
     })
-
 })
