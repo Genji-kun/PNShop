5ab7071622dfe8161571f2c70607bb551e115bd1
Genji-kun <87120829+Genji-kun@users.noreply.github.com>
2021-08-27 12:37:53 +0700

    hehe nho em

---
+++ js/js.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("#go-to-top").hide()
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 10) {
+            $(".cover").css({
+                "position": "fixed",
+                "top": 0,
+            })
+        } else{
+            $(".cover").css({
+                "position": "fixed",
+                "top": 0,
+            })
+        }
+        if ($(this).scrollTop() >= 435)
+            $("#go-to-top").show("slow")
+        else
+            $("#go-to-top").hide("slow")
+    })
+
+    $("#go-to-top").click(function() {
+        $("html, body").animate({
+            scrollTop:0
+        }, 1000);
+    })
+
+})
