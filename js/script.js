d02922a3241b5640cf9478c86f9571b23eb7e859
Genji-kun <87120829+Genji-kun@users.noreply.github.com>
2021-10-13 15:54:10 +0700

    BIn yêu em

--- js/script.js
+++ js/script.js
@@ -58,6 +58,29 @@ $(document).ready(function() {
         $(".exampleImg p").text($(this).children("a").children("p").text())
         $(".YMK-info").fadeIn(500)
         $("#black-space").fadeIn()
+        if(likeNum[$(this).children("a").attr("rel")] == true)
+        {
+            $(".like").addClass("rateLike")
+            $(".dislike").hide()
+            $(".like").children("a").text("Cảm ơn đánh giá của bạn!!")
+        }
+        else if(dislikeNum[$(this).children("a").attr("rel")] == true)
+        {
+            $(".dislike").addClass("rateDislike")
+            $(".like").hide()
+            $(".dislike").children("a").text("Cảm ơn đánh giá của bạn!!")
+        }
+        else
+        {
+            $(".dislike").removeClass("rateDislike")
+            $(".like").show()
+            $(".dislike").children("a").text("Không hữu ích ")
+            $(".dislike").children("a").append(`<i class="far fa-thumbs-down"></i>`)
+            $(".like").removeClass("rateLike")
+            $(".dislike").show()
+            $(".like").children("a").text("Hữu ích ")
+            $(".like").children("a").append(`<i class="far fa-thumbs-up"></i>`)
+        }
     })
     $(".exit").click(function() {
         $(this).parent().parent().fadeOut(500)
@@ -83,9 +106,67 @@ $(document).ready(function() {
             scrollTop: 2500
         },2000);
     })
+    var likeNum = []
+    for (var i = 0; i < $(".youmayknow").length; i++)
+    {
+        likeNum.push(false)
+    }
+    var dislikeNum = []
+    for (var i = 0; i < $(".youmayknow").length; i++)
+    {
+        dislikeNum.push(false)
+    }
     $(".like").click(function(){
-        var likeColor = $(this).css("background")
-        alert(likeColor)
-
+        var docImg = $(".YMK-detail img").attr("src")
+        var ymkImg = $(".youmayknow img")
+        for(var i = 0; i < $(".youmayknow img").length; i++)
+        {
+            if(docImg == $(ymkImg[i]).attr("src"))
+            { 
+                if(likeNum[i] == false)
+                {
+                    likeNum[i] = true
+                    $(".dislike").addClass("animate__fadeOutRight animate__animated")
+                    $(".like").addClass("rateLike animate__slideInLeft animate__animated")
+                    setTimeout(function(){
+                        $(".dislike").hide()
+                    }, 250)
+                    $(this).children("a").text("Cảm ơn đánh giá của bạn!!")
+                }
+                else
+                {   
+                    likeNum[i] = false
+                    $(this).removeClass("rateLike")
+                    $(".dislike").show(500)
+                    $(this).children("a").text("Hữu ích ")
+                    $(this).children("a").append(`<i class="far fa-thumbs-up"></i>`)
+                }
+            }
+        }
+    })
+    $(".dislike").click(function(){
+        var docImg = $(".YMK-detail img").attr("src")
+        var ymkImg = $(".youmayknow img")
+        for(var i = 0; i < $(".youmayknow img").length; i++)
+        {
+            if(docImg == $(ymkImg[i]).attr("src"))
+            { 
+                if(dislikeNum[i] == false)
+                {
+                    dislikeNum[i] = true
+                    $(this).addClass("rateDislike")
+                    $(".like").hide()
+                    $(this).children("a").text("Cảm ơn đánh giá của bạn!!")
+                }
+                else
+                {   
+                    dislikeNum[i] = false
+                    $(this).removeClass("rateDislike")
+                    $(".like").show(500)
+                    $(this).children("a").text("Không hữu ích ")
+                    $(this).children("a").append(`<i class="far fa-thumbs-down"></i>`)
+                }
+            }
+        }
     })
 })
