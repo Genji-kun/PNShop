fb20d477aec30c5543a7b686778679c263f31f5e
BaoNgan0206 <87120840+BaoNgan0206@users.noreply.github.com>
2021-12-06 22:00:52 +0700

    em nhớ Bin

--- js/script3.js
+++ js/script3.js
@@ -1,14 +1,44 @@
 $(document).ready(function () {
-    function squazeDiv() {
+    function squareDiv() {
         $(".photo").height($(".photo").width())
     }
-    squazeDiv()
+    squareDiv()
     $(window).resize(function () {
-        squazeDiv()
+        squareDiv()
+    })
+    var listnum = 0;
+    var maxlistnum = Math.ceil($(".photo").length/12) - 1
+    function changePage(listnum)
+    {
+        $(".photo").hide()
+        for (var i = listnum*12; i < listnum*12+12; i++)
+        {
+            $($(".photo")[i]).show()
+        }
+        $("html, body").animate({
+            scrollTop: $($(".title")[0]).offset().top
+        }, 1000);
+    }
+    $(".photo").hide()
+    changePage(listnum)
+    $(".prev-imglist").click(function(){
+        if (listnum != 0)
+        {
+            listnum--
+            changePage(listnum)
+        }
+    })
+    $(".next-imglist").click(function(){
+        if (listnum != maxlistnum)
+        {
+            listnum++
+            changePage(listnum)
+        }
     })
     $(".photo").click(function () {
         $("#black-space").show()
         $(".info-img").show();
+        $(".otherComments").html($(this).children(".hiddenComment").html())
         $(".info-img").children('a').attr('rel', $(this).children('a').attr('rel'));
         $(".info-img .frame img").attr('src', $(this).children().children('img').attr('src'));
         $(".info-img .download").attr('href', $(this).children().children('img').attr('src'));
@@ -42,7 +72,9 @@ $(document).ready(function () {
     $(".info-img .prev").click(function () {
         var thutu = $(".info-img").children('a').attr('rel') * 1
         thutu--;
-        $(".info-img").children('a').attr('rel', thutu);
+        $(".info-img").children('a').attr('rel', thutu)
+        var photo = $(".photo")
+        $(".otherComments").html($(photo[thutu-1]).children(".hiddenComment").html())
         $(".info-img .frame img").attr('src', `photo&video/photo${thutu}.jpg`);
         $(".info-img .download").attr('href', `photo&video/photo${thutu}.jpg`);
         $(".info-img .next").show()
@@ -68,7 +100,9 @@ $(document).ready(function () {
     $(".info-img .next").click(function () {
         var thutu = $(".info-img").children('a').attr('rel') * 1
         thutu++;
-        $(".info-img").children('a').attr('rel', thutu);
+        $(".info-img").children('a').attr('rel', thutu)
+        var photo = $(".photo")
+        $(".otherComments").html($(photo[thutu-1]).children(".hiddenComment").html())
         $(".info-img .frame img").attr('src', `photo&video/photo${thutu}.jpg`);
         $(".info-img .download").attr('href', `photo&video/photo${thutu}.jpg`);
         $(".info-img .prev").show()
@@ -124,26 +158,70 @@ $(document).ready(function () {
             heartlist[stt] = false;
             $(this).css({
                 "background-color": "#9e7260",
-                "color": "#e2c8be"
+                "color": "#e8d2ca"
             })
         }
     })
+    $(".commentShow").click(function(){
+        $(".comments").toggle();
+        $(".frame").toggleClass("frame-commentShow-clicked")
+        $(".commentShow").toggleClass("commentShow-clicked")
+    })
+    $("#sendComment").click(function () {
+        var stt = $(".info-img").children('a').attr('rel')*1
+        var newComment = $(".inputComment input").val()
+        if (newComment != "") {
+            $(".otherComments").prepend(`
+                    <div class="comment active">
+                        <div class="avt">
+                            <img src="images/user.png" alt="user"/>
+                        </div>
+                        <div class="commentContent">
+                            <p>${newComment}</p>
+                        </div>
+                    </div>
+            `)
+            var photo = $(".photo")
+            $(photo[stt-1]).children(".hiddenComment").prepend(`
+                <div class="comment">
+                            <div class="avt">
+                                <img src="images/user.png" alt="user"/>
+                            </div>
+                            <div class="commentContent">
+                                <p>${newComment}</p>
+                            </div>
+                        </div>
+            `)
+        }
+        setTimeout(function () {
+            $(".comments .comment").removeClass("active")
+        }, 1000)
+
+        $(".inputComment input").val("")
+    })
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 10) {
-            $(".cover").css({
-                "position": "fixed",
-                "top": 0,
-            })
+        var lane = $($(".title")[1]).offset().top - 10;
+        if ($(this).scrollTop() < lane) {
+            $(".imglist").addClass("list-active")
+            $(".vidlist").removeClass("list-active")
         } else {
-            $(".cover").css({
-                "position": "fixed",
-                "top": 0,
-            })
+            $(".vidlist").addClass("list-active")
+            $(".imglist").removeClass("list-active")
         }
         if ($(this).scrollTop() >= 435)
             $("#go-to-top").show("slow")
         else
             $("#go-to-top").hide("slow")
+    })
+    $(".imglist").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(".title")[0]).offset().top
+        }, 1000);
+    })
+    $(".vidlist").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(".title")[1]).offset().top
+        }, 1000);
     })
     $("#go-to-top").click(function () {
         $("html, body").animate({
